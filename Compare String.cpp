88116654835b05aa87a4fcf9562f3538d5605375
MantouOwO <50349257+MantouOwO@users.noreply.github.com>
2020-05-10 03:37:20 +0800

    Create Compare String.cpp
    :cat:

---
+++ Compare String.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstring>
+#define LEN 80
+using namespace std;
+
+bool loc(char *source ,char search,int source_size){
+	for(int i=0;i<source_size;i++){
+		if(source[i]==search){
+			source[i]='0';
+			return true;
+		}
+	}	
+	return false;
+}
+
+int main(){
+	char source[LEN];
+	char search[LEN];
+	
+	cout << "¿é¤JA¦r¦ê:";
+	cin >> source;
+	cout << "¿é¤JB¦r¦ê:";
+	cin >> search;
+	
+	int search_size = strlen(search);/*¦r¦êªø«×*/
+	int source_size = strlen(source);
+	
+	for(int i=0;i<search_size;i++){
+		if(loc(source ,search[i] ,source_size)==false){
+			cout << "false";
+			return 0;
+		}
+	}
+	
+	cout << "true";
+	return 0;
+}
